c70c0e01486a83aa6264b33477d28928acb5ea46
Brighten Zhang <bistromath25@gmail.com>
2023-09-13 18:32:25 -0400

    update form, grab form data

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 var path = require("path");
+const bodyParser = require('body-parser');
 
 const app = express();
 const BASE_URL = 'app url goes here'; // TODO set base url
@@ -9,6 +10,8 @@ const BASE_URL = 'app url goes here'; // TODO set base url
 app.engine('html', require('ejs').renderFile);
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
 app.use("/public", express.static(process.cwd() + "/public"));
 
 app.get("/", function(req, res) {
@@ -34,6 +37,11 @@ function copyLink() {
     document.getElementById("shortenedUrl").select()
     document.execCommand("copy");
 }
+
+app.post('/link', function(req, res) {
+    console.log(req.body);
+    res.sendFile(process.cwd() + "/public/index.html");
+});
 
 app.listen(3000, function() {
     console.log("Your app is listening on port 3000");
