626a464e7cd56808adaba9150d7d2a204daa059b
maltacc <108157439+maltacc@users.noreply.github.com>
2023-09-12 22:21:04 -0400

    Greated alphanumeric string generation function (#3)

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
-var path = require("path")
+var path = require("path");
 
 const app = express();
 const BASE_URL = 'app url goes here'; // TODO set base url
 
-app.engine('html', require('ejs').renderFile)
+app.engine('html', require('ejs').renderFile);
 
 app.use(cors());
 app.use("/public", express.static(process.cwd() + "/public"));
@@ -14,6 +14,16 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.get("/", function(req, res) {
     res.sendFile(process.cwd() + "/public/index.html");
 });
+
+// generate random alphanumeric string of length n
+function generate(n) {
+    var res = "";
+    var an="1234567890abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (var i = 0; i < n; i++) {
+        res += an.charAt(Math.floor((Math.random() * an.length)) % an.length);
+    }
+    return res;
+}
 
 app.listen(3000, function() {
     console.log("Your app is listening on port 3000");
