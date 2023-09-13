d13dbfb899782f935ed67b21ab98ca38923d66e1
Brighten Zhang <bistromath25@gmail.com>
2023-09-12 21:24:05 -0400

    index.html

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 var path = require("path")
+
+const app = express();
+const BASE_URL = 'app url goes here'; // TODO set base url
+
+app.engine('html', require('ejs').renderFile)
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.get("/", function(req, res) {
+    res.sendFile(process.cwd() + "/public/index.html");
+});
