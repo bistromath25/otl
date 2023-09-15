3f3cbddcae2210612c8961ab5ab025f2a71ec621
bistromath25 <71907421+bistromath25@users.noreply.github.com>
2023-09-15 19:22:33 -0400

    Get real link from database (#7)
    * rename post endpoint to /upload
    
    * insert into sqlite3 db
    
    * Update index.js
    
    don't close db
    
    * get real link from db, error and alreadyvisited pages

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
-const express = require("express");
-const cors = require("cors");
-const fs = require("fs");
-var path = require("path");
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs');
+var path = require('path');
+const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const json = require('stream/consumers');
+const db = new sqlite3.Database('./links.db');
+db.run('CREATE TABLE IF NOT EXISTS links(real TEXT, short TEXT, visited INTEGER)');
 
 const app = express();
-const BASE_URL = 'app url goes here'; // TODO set base url
+const BASE_URL = 'localhost:3000'; // TODO set base url
 
 app.engine('html', require('ejs').renderFile);
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use("/public", express.static(process.cwd() + "/public"));
+app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get("/", function(req, res) {
-    res.sendFile(process.cwd() + "/public/index.html");
+app.get('/', function(req, res) {
+    res.sendFile(process.cwd() + '/public/index.html');
 });
 
 function generate(n) {
     // generate random alphanumeric string of length n
-    var res = "";
-    const an="1234567890abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var res = '';
+    const an='1234567890abcdefhijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
     for (var i = 0; i < n; i++) {
         res += an.charAt(Math.floor((Math.random() * an.length)) % an.length);
     }
@@ -34,15 +38,47 @@ function saveUrl() {
 
 function copyLink() {
     // copy the shortened url
-    document.getElementById("shortenedUrl").select()
-    document.execCommand("copy");
+    document.getElementById('shortenedUrl').select()
+    document.execCommand('copy');
 }
 
-app.post('/link', function(req, res) {
-    console.log(req.body);
-    res.sendFile(process.cwd() + "/public/index.html");
+app.post('/upload', function(req, res) {
+    var real = req.body.real; // get the real url
+    var short = generate(5); // generate the short url
+    console.log(short);
+    db.run(`INSERT INTO links(real, short, visited) VALUES(?, ?, ?)`, [real, short, 0], function(e) {
+        if (e) {
+            return console.log(e);
+        }
+    });
+    res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.get('/:link', function(req, res) {
+    var short = req.params.link;
+    console.log(short);
+    const queryFetch = 'SELECT * FROM links WHERE short = ?';
+    const queryUpdate = 'UPDATE links SET visited = 1 WHERE short = ?'
+    db.get(queryFetch, [short], function(e, row) {
+        if (e) {
+            console.log(e);
+            res.sendFile(process.cwd() + '/public/error.html');
+        } else {
+            console.log(row);
+            if (row.visited === 0) { // only redirect if link has not yet been visited
+                res.redirect(row.real);
+            } else {
+                res.sendFile(process.cwd() + '/public/alreadyvisited.html');
+            }
+        }
+    });
+    db.run(queryUpdate, [short], function(e) {
+        if (e) {
+            return console.log(e);
+        }
+    });
 });
 
 app.listen(3000, function() {
-    console.log("Your app is listening on port 3000");
+    console.log('Your app is listening on port 3000');
 });
