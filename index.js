0c275874e695bd6487938bc0205c8070b660377b
Brighten Zhang <bistromath25@gmail.com>
2023-09-14 22:19:38 -0400

    get real link from db, error and alreadyvisited pages

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const fs = require('fs');
 var path = require('path');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
+const json = require('stream/consumers');
 const db = new sqlite3.Database('./links.db');
-db.run('CREATE TABLE IF NOT EXISTS links(real TEXT, short TEXT, visits INTEGER)');
+db.run('CREATE TABLE IF NOT EXISTS links(real TEXT, short TEXT, visited INTEGER)');
 
 const app = express();
-const BASE_URL = 'app url goes here'; // TODO set base url
+const BASE_URL = 'localhost:3000'; // TODO set base url
 
 app.engine('html', require('ejs').renderFile);
 
@@ -44,12 +45,38 @@ function copyLink() {
 app.post('/upload', function(req, res) {
     var real = req.body.real; // get the real url
     var short = generate(5); // generate the short url
-    db.run(`INSERT INTO links(real, short, visits) VALUES(?, ?, ?)`, [real, short, 0], function(e) {
+    console.log(short);
+    db.run(`INSERT INTO links(real, short, visited) VALUES(?, ?, ?)`, [real, short, 0], function(e) {
         if (e) {
             return console.log(e);
         }
     });
     res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.get('/:link', function(req, res) {
+    var short = req.params.link;
+    console.log(short);
+    const queryFetch = 'SELECT * FROM links WHERE short = ?';
+    const queryUpdate = 'UPDATE links SET visited = 1 WHERE short = ?'
+    db.get(queryFetch, [short], function(e, row) {
+        if (e) {
+            console.log(e);
+            res.sendFile(process.cwd() + '/public/error.html');
+        } else {
+            console.log(row);
+            if (row.visited === 0) { // only redirect if link has not yet been visited
+                res.redirect(row.real);
+            } else {
+                res.sendFile(process.cwd() + '/public/alreadyvisited.html');
+            }
+        }
+    });
+    db.run(queryUpdate, [short], function(e) {
+        if (e) {
+            return console.log(e);
+        }
+    });
 });
 
 app.listen(3000, function() {
