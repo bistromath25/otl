681692d80adf525ff2a17dd729d206273bcdb7d7
Alan Zheng <110870507+threeholestraw@users.noreply.github.com>
2023-09-23 21:29:46 -0400

    fixed favicon get request bug by setting a favicon

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ var path = require('path');
 const sqlite3 = require('sqlite3');
 const bodyParser = require('body-parser');
 const json = require('stream/consumers');
+const favicon = require('serve-favicon');
 const db = new sqlite3.Database('./links.db');
-db.run('CREATE TABLE IF NOT EXISTS links(real TEXT, short TEXT, visited INTEGER)');
+//const jsdom = require('jsdom');
+db.run('CREATE TABLE IF NOT EXISTS links(real TEXT, short TEXT, visited BOOLEAN)');
 
 const app = express();
 const BASE_URL = 'localhost:3000'; // TODO set base url
+
+const { JSDOM } = jsdom;
+const dom = new JSDOM('<input id="shortenedUrl" class="appearance-none block w-full bg-gray-200 text-gray-700 mr-3 border border-black-500 rounded py-2 px-3 leading-tight focus:outline-none focus:bg-white" type="text" placeholder="url-to-your-site">');
+global.document = dom.window.document;
 
 app.engine('html', require('ejs').renderFile);
 
@@ -17,6 +23,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use('/public', express.static(process.cwd() + '/public'));
+app.use(favicon(path.join(__dirname + '/public/opera.ico')));
 
 app.get('/', function(req, res) {
     res.sendFile(process.cwd() + '/public/index.html');
@@ -32,10 +39,6 @@ function generate(n) {
     return res;
 }
 
-function saveUrl() {
-    // add the url into the database
-}
-
 function copyLink() {
     // copy the shortened url
     document.getElementById('shortenedUrl').select()
@@ -46,27 +49,35 @@ app.post('/upload', function(req, res) {
     var real = req.body.real; // get the real url
     var short = generate(5); // generate the short url
     console.log(short);
-    db.run(`INSERT INTO links(real, short, visited) VALUES(?, ?, ?)`, [real, short, 0], function(e) {
+    
+    db.run(`INSERT INTO links(real, short, visited) VALUES(?, ?, ?)`, [real, short, false], function(e) {
         if (e) {
             return console.log(e);
         }
     });
-    res.sendFile(process.cwd() + '/public/index.html');
+
+    //const ret = document.getElementById('shortenedUrl'); 
+    //console.log(ret === undefined);
+    //ret.textContent = BASE_URL + '/' + short; 
+    
+    // res.sendFile(process.cwd() + '/public/index.html');
+    res.render(__dirname + '/public/index.html', {'placeholder': short});
 });
 
 app.get('/:link', function(req, res) {
     var short = req.params.link;
+    console.log(req.params);
     console.log(short);
     const queryFetch = 'SELECT * FROM links WHERE short = ?';
-    const queryUpdate = 'UPDATE links SET visited = 1 WHERE short = ?'
+    const queryUpdate = 'UPDATE links SET visited = true WHERE short = ?'
     db.get(queryFetch, [short], function(e, row) {
-        if (e) {
+        if (e || row === undefined) {
             console.log(e);
             res.sendFile(process.cwd() + '/public/error.html');
         } else {
             console.log(row);
             if (row.visited === 0) { // only redirect if link has not yet been visited
-                res.redirect(row.real);
+                res.redirect('//' + row.real);
             } else {
                 res.sendFile(process.cwd() + '/public/alreadyvisited.html');
             }
